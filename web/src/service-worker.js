e778c012b1e1c7c472778a237c7c707832000c19
Jakob Eriksson <jakob.eriksson@live.com>
2019-11-07 14:05:34 +0100

    tachyons

--- web/src/service-worker.js
+++ web/src/service-worker.js
@@ -8,3 +8,16 @@ workbox.core.clientsClaim(true);
 workbox.routing.registerNavigationRoute(
     workbox.precaching.getCacheKeyForURL('/index.html')
 );
+
+
+workbox.routing.registerRoute(
+    new RegExp('https://unpkg.com/tachyons@4/css/tachyons.min.css'),
+    new workbox.strategies.CacheFirst({
+        cacheName: 'tachyons-css',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+        ],
+    }),
+);
